df685abb769c39083b7eba75f25450bf39899489
GanaSasank <1nh19me032.ganasasankreddy@gmail.com>
2025-03-07 19:13:25 +0530

    Created frontend Folder

--- server.js
+++ server.js
@@ -3,4 +3,19 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 
+const app = express();
 
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Route testing sample
+app.get( "/", (req,res) => 
+    {
+            res.send("Hello, I am sample server for testing");
+    }
+);
+
+// Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running in PORT ${PORT}`));
